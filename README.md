04ada6fed90b9258706ee40fa35a7eb13f88969c
Lloyd Dilley <lloyd@dilley.me>
2018-04-12 22:48:06 -0400

    Added event handling and tweet capability

--- README.md
+++ README.md
@@ -4,7 +4,26 @@ gwitter
 
 GitHub to Twitter hook glue
 
-This project is not usable yet! Events still need to be properly handled and posted to Twitter.
+Gwitter is primarily being written for use with the [SEGS Project](https://github.com/Segs/Segs) for Twitter
+and Discord notifications. As such, only pull request/merge and release events are currently handled considering
+the needs of the aforementioned project. You will need to manually handle other specific event types that you want
+posted to Twitter as a result. Support for more events will likely be added for posterity at some point. For a
+list of available events supported by the GitHub API, please see the following:
+https://developer.github.com/webhooks/#events
+https://developer.github.com/v3/activity/events/types/
 
-For added security, you can permit traffic on your firewall for just the hook networks referenced here:
-https://api.github.com/meta
+Gwitter requires the use of Twitter consumer and access tokens for authentication and authorization. These can
+be procured after creating a Twitter application at: https://apps.twitter.com/
+
+For added security, you can optionally:
+* Permit traffic on your firewall for just the hook networks referenced here: https://api.github.com/meta
+* Generate a GitHub hook secret for validation using: `ruby -rsecurerandom -e 'puts SecureRandom.hex(20)'`
+* Make use of SSL certificates which can be generated using Let's Encrypt or another CA. You may also generate
+self-signed certificates using:
+`openssl req -x509 -nodes -newkey rsa:2048 -keyout server.key -out server.crt -days 365`
+Note: If you are utilizing self-signed certificates, you must disable SSL verification for the GitHub hook.
+
+Gwitter has the following requirements:
+Ruby (including [JRuby](http://jruby.org/)) >=2.2
+[Sinatra Gem](https://github.com/sinatra/sinatra)
+[Twitter Gem](https://github.com/sferik/twitter)
--- gwitter.rb
+++ gwitter.rb
@@ -22,19 +22,26 @@ if RUBY_VERSION < '2.2'
   exit!
 end
 
+# Dependencies
 require 'json'
 require 'openssl'
-require 'sinatra'
+require 'sinatra/base'
 require 'twitter'
 require 'webrick'
 require 'webrick/https'
 require 'yaml'
 
+# Constants and globals (many globals are set in parse_config())
 GWITTER_VERSION = '1.0'
+TWEET_LIMIT = 280 # chars
+$log_dir = 'logs'
+$log_name = 'gwitter.log'
+$config_name = 'config.yml'
 
+# Reads $config_name and stores options in globals
 def parse_config
   begin
-    config_file = YAML.load_file('config.yml')
+    config_file = YAML.load_file($config_name)
   rescue => error_msg
     puts "Unable to open config.yml file: #{error_msg}"
     exit!
@@ -49,10 +56,17 @@ def parse_config
   $consumer_secret = config_file['consumer_secret']
   $access_token = config_file['access_token']
   $access_secret = config_file['access_secret']
+  $become_daemon = config_file['become_daemon']
   $debug_mode = config_file['debug_mode']
 
-  # Validate values
-  unless $listen_host.nil?
+  validate_config
+end
+
+# Validates values read from parse_config()
+def validate_config
+  if $listen_host.nil?
+    $listen_host = '0.0.0.0'
+  else
     unless valid_hostname?($listen_host) || valid_address?($listen_host)
       puts 'listen_host value is not a valid hostname or IP address!'
       exit!
@@ -90,10 +104,16 @@ def parse_config
   end
 
   $use_ssl = false if $use_ssl.nil?
+  $become_daemon = false if $become_daemon.nil?
   $debug_mode = false if $debug_mode.nil?
 
   if $use_ssl.to_s != 'true' && $use_ssl.to_s != 'false'
     puts 'use_ssl value should be set to either \'true\' or \'false\'.'
+    exit!
+  end
+
+  if $become_daemon.to_s != 'true' && $become_daemon.to_s != 'false'
+    puts 'become_daemon value should be set to either \'true\' or \'false\'.'
     exit!
   end
 
@@ -103,6 +123,10 @@ def parse_config
   end
 end
 
+# Validates hostnames
+# @param hostname [string] the hostname to validate
+# @return [true] if hostname is valid
+# @return [false] if hostname is invalid
 def valid_hostname?(hostname)
   if hostname =~ /^(?:[a-zA-Z0-9]+(?:\-*[a-zA-Z0-9])*\.)+[a-zA-Z]{2,6}$/i
     return true
@@ -111,6 +135,10 @@ def valid_hostname?(hostname)
   end
 end
 
+# Validates IP addresses
+# @param address [string] the IP address to validate
+# @return [true] if address is valid
+# @return [false] if address is invalid
 def valid_address?(address)
   # IPv4
   if address =~ /^\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b$/
@@ -129,16 +157,90 @@ def valid_address?(address)
   end
 end
 
+# Logs text entry to $log_dir/$log_name
+# @param text [string] text entry to log to file
 def log_write(text)
-  log_dir = 'logs'
-  Dir.mkdir(log_dir) unless File.exists?(log_dir)
-  log_file = File.open("#{log_dir}/gwitter.log", 'a')
-  log_file.puts "#{Time.now.asctime} - #{text}"
+  Dir.mkdir($log_dir) unless File.exists?($log_dir)
+  log_file = File.open("#{$log_dir}/#{$log_name}", 'a')
+  time_stamp = Time.now.strftime "[%m/%d/%Y %H:%M:%S]"
+  log_file.puts "#{time_stamp} #{text}"
   log_file.close
   rescue
     puts 'Unable to write log file!'
 end
 
+# Verifies hook payload using hashing with $github_secret
+# @param payload_body [string] body of hook payload to verify
+# @return [string] HTTP error code 500 if signature does not match
+def verify_signature(payload_body)
+  signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), $github_secret, payload_body)
+  unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
+    return halt 500, "Signature mismatch!"
+    log_write 'Signature mismatch!'
+  end
+end
+
+# Handles possible GitHub event types
+# @param push_event [string] event data to grok
+# @return [string] message to tweet
+def handle_event push_event
+  message = ""
+  case
+    # Release
+    when push_event.key?('release')
+      release_name = push_event['release']['name']
+      release_tag = push_event['release']['tag_name']
+      release_url = push_event['release']['html_url']
+      release_body = push_event['release']['body']
+      message += "#{release_name} #{release_tag}\r\n"
+      message += "#{release_url}\r\n"
+      message += "#{release_body}"
+    # Pull request (merge only)
+    when push_event.key?('pull_request') && push_event['action'] == 'closed' && push_event['merged'] == 'true'
+      pr_title = push_event['pull_request']['title']
+      pr_url = push_event['pull_request']['html_url']
+      pr_body = push_event['pull_request']['body']
+      message += "#{pr_title}\r\n"
+      message += "#{pr_url}\r\n"
+      message += "#{pr_body}"
+    # ToDo: Handle more events and make them configurable
+    # Unhandled event
+    else
+      message = nil
+      log_write('Unhandled event encountered.')
+  end
+  return message
+end
+
+# Tweets a message
+# @param message [string] message to tweet
+def tweet(message)
+  # Configure Twitter client auth
+  client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = $consumer_key
+    config.consumer_secret     = $consumer_secret
+    config.access_token        = $access_token
+    config.access_token_secret = $access_secret
+  end
+
+  # Links should automagically be shortened by the t.co service
+  # Acquiring the shortened link maximum length is rate limited:
+  # https://developer.twitter.com/en/docs/developer-utilities/configuration/api-reference/get-help-configuration
+  client_conf = client.configuration
+
+  # If one particular shortened link maximum length (HTTP versus HTTPS) is higher, use the higher value to be safe.
+  link_limit = client_conf.short_url_length <= client_conf.short_url_length_https ? client_conf.short_url_length : client_conf.short_url_length_https
+
+  # Truncate message if it exceeds limit
+  if (message.length - link_limit) > TWEET_LIMIT
+    message = message[TWEET_LIMIT - link_limit]
+  end
+
+  # Submit tweet
+  client.update(message)
+end
+
+# Main
 puts "Gwitter #{GWITTER_VERSION}"
 log_write "Gwitter #{GWITTER_VERSION}"
 puts 'Parsing config.yml...'
@@ -146,24 +248,86 @@ log_write 'Parsing config.yml...'
 parse_config
 puts 'config.yml successfully parsed.'
 log_write 'config.yml successfully parsed.'
+Thread.abort_on_exception = true if $debug_mode.to_s == 'true'
 puts 'Starting web service...'
 log_write 'Starting web service...'
 
-set :bind, $listen_host unless $listen_host.nil?
-set :port, $listen_port
-
-post '/payload' do
-  #request.body.rewind
-  payload_body = request.body.read
-  verify_signature(payload_body) unless $github_secret.nil?
-  push = JSON.parse(payload_body)
-  #push = JSON.parse(params[:payload])
-  puts push.inspect
-  # ToDo: Continue to grok payload here and formulate Twitter post accordingly based on event(s)
-  # Also use WEBrick and OpenSSL to read in certs and handle SSL connections
+# This class is where all the magic happens
+class Gwitter < Sinatra::Base
+  post '/payload' do
+    #request.body.rewind
+    log_write "Connection received from #{request.ip} for: #{request.path}"
+    payload_body = request.body.read
+    verify_signature(payload_body) unless $github_secret.nil?
+    push_event = JSON.parse(payload_body)
+    #push_event = JSON.parse(params[:payload])
+    log_write "Payload contents: #{push_event.inspect}"
+    message = handle_event push_event
+    tweet(message) unless message.nil?
+  end
 end
 
-def verify_signature(payload_body)
-  signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), $github_secret, payload_body)
-  return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
+# Configure WEBrick logging
+access_log = File.new("#{$log_dir}/#{$log_name}", 'a')
+logger = WEBrick::Log::new(access_log, WEBrick::Log::DEBUG)
+logger.time_format = "[%m/%d/%Y %H:%M:%S]"
+access_log.sync = true # Write logs immediately to disk. Otherwise, logs may be buffered until program terminates.
+
+# Set PID file
+pid_file = 'gwitter.pid'
+
+# Check if JRuby is being used and $become_daemon is set
+if RUBY_PLATFORM == 'java' && $become_daemon.to_s == 'true'
+  puts 'JRuby does not support fork! Running in foreground...'
+  log_write 'JRuby does not support fork! Running in foreground...'
+  $become_daemon = false
 end
+
+# Use a CA certificate if available
+ca_file = 'ca.crt' if File.exists?('ca.crt')
+
+# Use TLS, disable weak ciphers, prevent CRIME (CVE-2012-4929), etc.
+ssl_options = OpenSSL::SSL::OP_NO_SSLv3 + OpenSSL::SSL::OP_NO_SSLv2
+
+# OpenSSL::SSL::OP_NO_COMPRESSION does not seem to be defined in JRuby 9.1.16.0 hence the check below.
+ssl_options += OpenSSL::SSL::OP_NO_COMPRESSION if defined?(OpenSSL::SSL::OP_NO_COMPRESSION)
+ssl_ciphers = 'TLSv1.2:!aNULL:!eNULL:!AES128'
+
+# BindAddress does not work as expected, so use Host too.
+if $use_ssl
+  webrick_options = {
+    :BindAddress          => $listen_host,
+    :Host                 => $listen_host,
+    :Port                 => $listen_port,
+    :Logger               => logger,
+    :AccessLog            => [[logger, WEBrick::AccessLog::COMBINED_LOG_FORMAT]],
+    :DocumentRoot         => 'payload',
+    :SSLEnable            => true,
+    #:SSLVerifyClient      => OpenSSL::SSL::VERIFY_NONE,
+    :SSLCertificate       => OpenSSL::X509::Certificate.new(File.open('server.crt').read),
+    :SSLPrivateKey        => OpenSSL::PKey::RSA.new(File.open('server.key').read),
+    :SSLCACertificateFile => ca_file,
+    :SSLCertName          => [['CN',WEBrick::Utils::getservername]],
+    :SSLOptions           => ssl_options,
+    :SSLCiphers           => ssl_ciphers,
+    :SSLVersion           => :TLSv1_2,
+    :daemonize            => $become_daemon,
+    :pid                  => File.expand_path(pid_file),
+    :app                  => Gwitter
+  }
+else
+  webrick_options = {
+    :BindAddress          => $listen_host,
+    :Host                 => $listen_host,
+    :Port                 => $listen_port,
+    :Logger               => logger,
+    :AccessLog            => [[logger, WEBrick::AccessLog::COMBINED_LOG_FORMAT]],
+    :DocumentRoot         => 'payload',
+    :SSLEnable            => false,
+    :daemonize            => $become_daemon,
+    :pid                  => File.expand_path(pid_file),
+    :app                  => Gwitter
+  }
+end
+
+Rack::Server.start webrick_options
